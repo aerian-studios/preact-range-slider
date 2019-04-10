d834e776b552c4fd899867ff347ccc9fa26686cc
Darren Ivey <darren.ivey@aerian.com>
2019-04-10 13:13:55 +0100

    Update stylings

--- src/components/AbstractSlider.tsx
+++ src/components/AbstractSlider.tsx
@@ -244,14 +244,16 @@ abstract class AbstractSlider<
 						min={minValue}
 						classesPrefix={classesPrefix}
 					/>
-					{children}	
-					<span class={`${classesPrefix}tip
-						${classesPrefix}tip-dragging-${this.state.dragging}
-						${classesPrefix}tip-display-${true}`}
-						style={{left: `${this.calcOffset( this.state.toolTipValue )}%`}} >
-						{formatTime(this.state.toolTipValue | 0)}
-						{ !this.state.dragging && <span className="tip-indicator" /> }
-					</span>
+					{children}
+					{ this.state.toolTipDisplay &&
+						<span class={`${classesPrefix}tip
+							${classesPrefix}tip-dragging-${this.state.dragging}
+							${classesPrefix}tip-display-${true}`}
+							style={{left: `${this.calcOffset( this.state.toolTipValue )}%`}} >
+							{formatTime(this.state.toolTipValue | 0)}
+							{ !this.state.dragging && <span className="tip-indicator" /> }
+						</span>
+					}
 				</div>
 			</div>
 		);
